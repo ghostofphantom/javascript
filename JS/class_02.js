dde97cc3c3ac43482418d1fea32dda0db768f6de
ghostofphantom <itsmehassxn@gmail.com>
2025-07-14 16:34:45 +0500

    functions

--- JS/class_02.js
+++ JS/class_02.js
@@ -1,41 +1,41 @@
 
 
-let const var
+// let const var
 
-OPERATORS
+// OPERATORS
 
-( = Assignment Operator )
-( == Comparison Opertor )
-( === Comparison Opertor with data type )
-( 5 > 4 Greater Than )
-( 5 < 4 Less Than  )
-( 5 <= 4 Less Than  )
-( 5 >= 4 Less Than  )
-( 5 != 4 Not Equal  )
-( 5 !== 4 Not Equal with data type  )
+// ( = Assignment Operator )
+// ( == Comparison Opertor )
+// ( === Comparison Opertor with data type )
+// ( 5 > 4 Greater Than )
+// ( 5 < 4 Less Than  )
+// ( 5 <= 4 Less Than  )
+// ( 5 >= 4 Less Than  )
+// ( 5 != 4 Not Equal  )
+// ( 5 !== 4 Not Equal with data type  )
 
-+
--
-/
-%
-*
-
-
-const myname = "5"
-const myage = 5;
-
-const firstNum = 5;
-const secNum = 5;
-document.write( firstNum + secNum  + "<br />" )
-document.write( firstNum - secNum   + "<br />")
-document.write( firstNum * secNum   + "<br />")
-document.write( firstNum / secNum   + "<br />")
-document.write( firstNum % secNum   + "<br />")
+// +
+// -
+// /
+// %
+// *
 
 
-const h1 = document.getElementById("heading")
- const btn = document.getElementById("button");
+// const myname = "5"
+// const myage = 5;
 
- btn.addEventListener("click" , () => {
-    h1.style.color = "red"
- })
+// const firstNum = 5;
+// const secNum = 5;
+// document.write( firstNum + secNum  + "<br />" )
+// document.write( firstNum - secNum   + "<br />")
+// document.write( firstNum * secNum   + "<br />")
+// document.write( firstNum / secNum   + "<br />")
+// document.write( firstNum % secNum   + "<br />")
+
+
+// const h1 = document.getElementById("heading")
+//  const btn = document.getElementById("button");
+
+//  btn.addEventListener("click" , () => {
+//     h1.style.color = "red"
+//  })
---
+++ JS/functions_01.js
@@ -0,0 +1,15 @@
+
+// parameters
+function Hello(num1 , num2){
+    // alert("THIS IS JS FUNCTION")
+    document.write(num1 + num2)
+    // document.write("jjlksaj ;lfkjaslfjalkf jdsalkfjsalkfjalksjf alksd")
+}
+
+Hello(10 , 10)
+// ARGUMENTS
+Hello(50 , 30)
+Hello(50 , 30)
+Hello(50 , 30)
+Hello(50 , 7)
+
