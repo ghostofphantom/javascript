1d66b33b89d592a788e540ba4bdb5044021857de
ghostofphantom <itsmehassxn@gmail.com>
2025-07-25 17:10:37 +0500

    event listner

--- JS/dom.js
+++ JS/dom.js
@@ -1,4 +1,4 @@
-let para = document.querySelector("#para")
+
 
 let box = document.getElementById("box");
 
@@ -10,17 +10,41 @@ let button = document.querySelector("button")
 
 // console.log(heading)
 
-function change(){
-    box.style.background = 'violet';
-    box.style.width = "300px"
-    box.style.padding = "50px" 
-    box.style.borderRadius = "20px" 
-    heading.style.color = 'black'
-    para.innerHTML = "This is paragraph from javascirpt"
-    para.style.color ="orange"
-    button.style.margin = "14px 08px"
-    button.style.padding = "06px"
-    button.style.background = "brown"
-    button.style.color = "black"
+// function change(){
+//     box.style.background = 'violet';
+//     box.style.width = "300px"
+//     box.style.padding = "50px" 
+//     box.style.borderRadius = "20px" 
+//     heading.style.color = 'black'
+//     para.innerHTML = "This is paragraph from javascirpt"
+//     para.style.marginLeft = "10px"
+//     para.style.color ="orange"
+//     button.style.margin = "14px 08px"
+//     button.style.padding = "06px"
+//     button.style.background = "brown"
+//     button.style.color = "black"
+    
+// }
 
-}
+// let paragraph = document.querySelector(".paragraph")
+
+// paragraph.style.background = "orange"
+
+// console.table(paragraph)
+
+// paragraph.style.backgound = "red"
+
+
+// button.addEventListener("onmouseover" , () => {
+
+//  let para = document.querySelector("#para")
+// para.style.background = "red"
+
+
+// })
+
+// function Hello(){
+//     alert("this is alert")
+// }
+
+
