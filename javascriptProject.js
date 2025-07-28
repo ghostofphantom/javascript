7b65cb034808ab9d4a4c1852f9503b474f108ea5
ghostofphantom <itsmehassxn@gmail.com>
2025-07-28 17:13:14 +0500

    Form

---
+++ javascriptProject.js
@@ -0,0 +1,77 @@
+
+
+// let button = document.querySelector("button")
+// let input  = document.querySelector("input")
+
+
+// button.addEventListener("click" , () => {
+    
+//     if(input.type === "password"){
+//         input.type = "text"
+//         button.innerHTML = "Hide"
+//     }else{
+//         input.type = "password"
+//         button.innerHTML = "Show"
+//     }
+
+
+// })
+
+
+// let add = document.getElementById("add")
+// let remove = document.getElementById("remove")
+// let toggle = document.getElementById("toggle")
+// let para = document.querySelector("p")
+
+
+// add.addEventListener("click" , () => {
+//     para.classList.add("paraStyle")
+// })
+// remove.addEventListener("click" , () => {
+//     para.classList.remove("paraStyle")
+// })
+// toggle.addEventListener("click" , () => {
+//     para.classList.toggle("paraStyle")
+// })
+
+// let input = document.querySelector("input")
+// let button = document.querySelector("button")
+
+// button.addEventListener("click" , () => {
+//     if(input.type === "password"){
+//          input.type = "text"
+//          button.innerHTML = "Hide Password"
+//     }
+//     else{
+//         input.type = "password"
+//         button.innerHTML = "View Password"
+//         }
+// })
+
+
+
+let add = document.getElementById("add")
+let remove = document.getElementById("remove")
+let para = document.querySelector("p")
+
+add.addEventListener("click" , () => {
+    para.classList.add("paraStyle")
+})   
+remove.addEventListener("click" , () => {
+    para.classList.remove("paraStyle")
+})   
+toggle.addEventListener("click" , () => {
+    para.classList.toggle("paraStyle")
+})   
+
+
+
+
+
+
+
+
+
+
+
+
